f4e3e51a38e1b10550cf85213bed1e46a474c342
4e4o <sks_f@mail.ru>
2022-04-06 11:20:05 +0700

    updated boost_base

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "Application.hpp"
 
 int main(int argc, char** argv) {
-    std::unique_ptr<Application> app(new Application(argc, argv));
+    TAApplication app(new Application(argc, argv));
     return app->exec();
 }
